5d6916f594b49acc3ce9667c183591edf5d379c7
zainabsalman <zsalman380@gmail.com>
2026-05-12 15:26:21 +0300

    fooditems

--- zaya-class-code/zaya_project/settings.py
+++ zaya-class-code/zaya_project/settings.py
@@ -126,3 +126,7 @@ STATIC_URL = "static/"
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "/"  # after successful login
 LOGOUT_REDIRECT_URL = "login"  # after logout
+
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
